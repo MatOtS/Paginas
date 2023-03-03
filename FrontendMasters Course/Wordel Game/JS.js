3e19453ebf5c00508adeac4b7596aca7b6586bb8
MacMatias <matu9901@gmail.com>
2023-03-03 21:06:16 +0100

    Update

--- FrontendMasters Course/Wordel Game/JS.js
+++ FrontendMasters Course/Wordel Game/JS.js
@@ -10,6 +10,10 @@ setTimeout("hide()", 2000);
 
 //No entiendo por qué me marca event como depreciated, se puede usar
 
+function CrearListas(lista) {
+
+}
+
 function esLetra() {
     const keyCodes = window.event.keyCode
     if (keyCodes>64 && keyCodes<91) {
@@ -20,23 +24,37 @@ function esLetra() {
     }
 }
 
+function EnterValidar(number) {
+
+
+}
+
+function BorrarLetra() {
+    document.querySelectorAll(".letra")[j].innerHTML = ""
+    contador--
+    j--
+    console.log(contador)
+
+}
+
+
 function display() {
     letra = esLetra()
     command = event.key
     if (contador<5 && letra != "" ) {
         for (i=0;i<contador;i++);contador++;j++; {
             console.log(letra)
-            console.log(i)
-            console.log(contador)
-            console.log(j)
             document.querySelectorAll(".letra")[j].innerHTML = letra
         }
     }
-    else if (command == "Enter") {
-        contador = 0
+    else if (contador == 5 && letra != "") {
         console.log(contador)
+        document.querySelectorAll(".letra")[j].innerHTML = letra
     }
-    else if (command == "backspace") {
-        console.log(command)
+    else if (command == "Enter") {
+        EnterValidar(contador, )
+    }
+    else if (command == "Backspace") {
+        BorrarLetra()
     }
 }
