41673bb0be4338262d1645608be033837bc8ab9c
Matias <matu9901@gmail.com>
2023-10-31 00:07:26 +0100

    Update

--- Proyects/Intro component with sign-up form/main.js
+++ Proyects/Intro component with sign-up form/main.js
@@ -1,32 +1,39 @@
 const mensajeRojo = document.getElementsByClassName("error-mnj")
+window.addEventListener("keydown", EnterButton)
 document.querySelector(".submit").addEventListener("click", function Submit() {
     const name = document.getElementsByName("name")[0].value
     const lastName = document.getElementsByName("lastname")[0].value
     const email = document.getElementsByName("email")[0].value
     const pw = document.getElementsByName("pw")[0].value
-    console.log("Funciona")
-    console.log(name)
-    console.log(lastName)
-    console.log(email)
-    console.log(pw)
 
     Checker(name,0)
     Checker(lastName,1)
     CheckerEmail(email,2)
     Checker(pw,3)
+})
+
+function EnterButton() {
+    command = event.key
+    if (command == "Enter") {
+        console.log("Enter")
+    }
 }
-)
 
 function Checker(string,number) {
     if (string.length === 0) {
-        console.log(mensajeRojo)
         mensajeRojo[number].style.visibility = "visible";
+    }
+    else {
+        mensajeRojo[number].style.visibility = "hidden";
     }
 }
 
 function CheckerEmail(string,number) {
     if (string.length === 0 || !(string.includes("@"))) {
         mensajeRojo[number].style.visibility = "visible";
+    }
+    else {
+        mensajeRojo[number].style.visibility = "hidden";
     }
 }
 
