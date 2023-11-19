8591f723714e941385c04ebe700040e65c2e46fe
Mat <76122487+MatOtS@users.noreply.github.com>
2023-11-19 20:15:49 +0100

    asdasd

--- Proyects/Intro-component-with-signUp-form/main.js
+++ Proyects/Intro-component-with-signUp-form/main.js
@@ -33,7 +33,7 @@ function Checker(string,number) {
 function CheckerEmail(string,number) {
     valor = string[0].value
     console.log(string)
-    if (valor.length === 0 && !(string.includes("@"))) {
+    if (valor.length === 0 || !(string.includes("@"))) {
         mensajeRojo[number].style.visibility = "visible";
         string[0].style.borderColor = "red";
         setTimeout(function(){
